86b2bf6ebcece4d6d69cbbebaeceea4274b8e625
Rafiul Siddique <rafiulsiddique301@gmail.com>
2025-09-05 19:28:45 +0600

    Drop-Off Points Index Page

---
+++ Green Cycle/Migrations/202509051316335_AddDropOffPoints1.cs
@@ -0,0 +1,16 @@
+﻿namespace Green_Cycle.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDropOffPoints1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
