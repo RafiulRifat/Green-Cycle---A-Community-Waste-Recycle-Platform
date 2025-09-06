acd44f9712f40907b3037141806432b2ccf52e69
Marium2022 <prity.cse.20210204098@aust.edu>
2025-09-06 15:29:34 +0600

    Impact Dashboard

--- Green Cycle/Models/ViewModels/Dashboard/DashboardViewModel.cs
+++ Green Cycle/Models/ViewModels/Dashboard/DashboardViewModel.cs
@@ -1,11 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace Green_Cycle.Models.ViewModels.Dashboard
+namespace GreenCycle.Models.ViewModels
 {
     public class DashboardViewModel
     {
+        public double TotalCO2SavedKg { get; set; }
+        public int ItemsDiverted { get; set; }
+        public int ChallengesJoined { get; set; }
+
+        public List<WeeklyPoint> WeeklyCO2 { get; set; } = new List<WeeklyPoint>();
+        public List<ActivityItem> Recent { get; set; } = new List<ActivityItem>();
+    }
+
+    public class WeeklyPoint
+    {
+        public string WeekLabel { get; set; }  // e.g., "Jun 23"
+        public double Kg { get; set; }         // value in kg
+    }
+
+    public class ActivityItem
+    {
+        public DateTime Date { get; set; }
+        public string Label { get; set; }      // e.g., "Plastic Bottle"
+        public string Material { get; set; }   // e.g., "Plastic"
+        public double SavedCO2Kg { get; set; } // e.g., 0.08
     }
 }
