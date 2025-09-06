1dd3fe0cb7d740cec4682cd448f6bb8e3deae3ff
Marium2022 <prity.cse.20210204098@aust.edu>
2025-09-06 14:54:54 +0600

    Recognition(Index,Result)

---
+++ Green Cycle/Models/ViewModels/RecognitionResultViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GreenCycle.Models.ViewModels
+{
+    public class RecognitionResultViewModel
+    {
+        public string Label { get; set; }
+        public double Confidence { get; set; }      // 0.92 => 92%
+        public string Material { get; set; }
+        public double SavedCO2 { get; set; }        // kilograms
+        public string[] Instructions { get; set; }
+        public DateTime ScannedAt { get; set; }
+        public string PreviewImagePath { get; set; } // ~/Content/uploads/...
+    }
+}
